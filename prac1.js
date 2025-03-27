35afc2a0ae7f101e3b057b5ca957a6072ceee929
ushapaul <ushapaul2326@gmail.com>
2025-03-27 22:27:06 +0530

    profile

---
+++ prac1.js
@@ -0,0 +1,18 @@
+// const product={
+//     name:"Ball pen",
+//     rating:4,
+//     offer:5,
+//     price:270,
+// };
+// console.log(product);
+const insta_profile={
+    username:"florence paul",
+    isFollow:false,
+    followers:500,
+    following: 600,
+
+};
+console.log(insta_profile);
+console.log(typeof insta_profile);
+console.log(typeof insta_profile["following"]);
+
