0707c53d4f3dd526d5e9df1ae0d07721bf0dfea1
ushapaul <ushapaul2326@gmail.com>
2025-03-28 13:11:18 +0530

    practice

---
+++ prac2.js
@@ -0,0 +1,23 @@
+//practice Qs1
+// let num=prompt("Enter a number");
+// if(num%5===0){
+//     console.log(num,"is multiple of 5");
+// }else{
+//     console.log(num,"not a multiple of a 5");
+// }
+//ppractice Qs2
+let score = 75;
+let grade;
+if(score >= 90 && score <= 100) {
+    console.log("grade is A");
+} else if(score >= 70 && score <= 89) {
+    console.log("grade is B");
+}else if(score >= 60 && score <= 69) {
+    console.log("grade is C");
+}else if(score >= 50 && score <= 59) {
+    console.log("grade is D");
+}else{
+    console.log("grade is F");
+}
+
+
